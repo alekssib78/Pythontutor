d1e12f0b0c4d8c62c96c2795151243551c06b985
Unknown <alekssib78@gmail.com>
2021-11-23 20:33:31 +0300

    Шнурки

---
+++ Шнурки.py
@@ -0,0 +1,8 @@
+klass_a = int(input())
+klass_b = int(input())
+klass_v = int(input())
+part_a = (klass_a // 2) + (klass_a % 2)
+part_b = (klass_b // 2) + (klass_b % 2)
+part_v = (klass_v // 2) + (klass_v % 2)
+print(part_a + part_b + part_v)
+
